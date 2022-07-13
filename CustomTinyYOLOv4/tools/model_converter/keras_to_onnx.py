5f8551bba1084de711ce8c8de8d8f900746fac85
Alessandro Resta <77611890+Alessandrus00@users.noreply.github.com>
2022-07-13 20:55:41 +0200

    Update keras_to_onnx.py

--- CustomTinyYOLOv4/tools/model_converter/keras_to_onnx.py
+++ CustomTinyYOLOv4/tools/model_converter/keras_to_onnx.py
@@ -4,8 +4,14 @@ import os
 os.environ['TF_KERAS'] = '1'
 import keras2onnx
 
-model = tf.keras.models.model_from_json(open('model_data/yolov4_tiny.json').read(), custom_objects={'tf': tf})
-weights_path = input("model weights path: ")
+model_file = open('model_data/yolov4_tiny.json').read()
+model = tf.keras.models.model_from_json(model_file, custom_objects={'tf': tf})
+weights_path = input('model weights path: ')
 model.load_weights(weights_path)
-onnx_model = keras2onnx.convert_keras(model, model.name, channel_first_inputs=['input_1'], target_opset=9)
+onnx_model = keras2onnx.convert_keras(
+    model, 
+    model.name, 
+    channel_first_inputs=['input_1'], 
+    target_opset=9
+    )
 keras2onnx.save_model(onnx_model, 'model_data/yolov4_tiny.onnx')
